4ea63e3c7afd353d2ca7d9f927350429d4b583e9
fayzaya <fayzayaganangputra20@gmail.com>
2025-10-27 15:26:22 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ⚙️ Ini penting untuk GitHub Pages
+  base: '/jadwalmobil/',
+
+  // Opsional, tapi tetap kamu punya ini untuk optimisasi
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
